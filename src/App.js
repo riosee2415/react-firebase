9c1c59491fddc1a4447ac8b9798af3945642d636
4leaf.ysh <4leaf.ysh@gmail.com>
2019-12-29 21:53:27 +0900

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,70 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    tasks: [
+      { todo: "work 1" },
+      { todo: "work 2" },
+      { todo: "work 3" },
+      { todo: "work 4" }
+    ],
+    task: ""
+  };
+
+  onClickHandler = e => {
+    e.preventDefault();
+
+    if (this.state.task.length === 0) {
+      return;
+    }
+
+    const task = {
+      todo: this.state.task
+    };
+
+    const tasks = [...this.state.tasks, task];
+
+    this.setState({
+      tasks,
+      task: ""
+    });
+  };
+
+  // to way binding
+  onChangeHandler = e => {
+    this.setState({
+      task: e.target.value
+    });
+  };
+
+  render() {
+    const { task } = this.state;
+
+    const taskDisplay = this.state.tasks.map((task, index) => {
+      return (
+        <div key={index}>
+          <p>{task.todo}</p>
+          <button>DELETE</button>
+        </div>
+      );
+    });
+
+    console.log(taskDisplay);
+    return (
+      <div className="App">
+        {/* INPUT */}
+        <div>
+          <form>
+            <input value={task} onChange={this.onChangeHandler}></input>
+            <button onClick={this.onClickHandler}>SAVE</button>
+          </form>
+        </div>
+
+        {/* OUTPUT */}
+        <div>{taskDisplay}</div>
+      </div>
+    );
+  }
 }
 
 export default App;
